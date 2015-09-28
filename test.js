9ebc4364263980f7984dac96091b7632b9f2f07d
ragingwind <ragingwind@gmail.com>
2015-09-29 07:02:05 +0900

    Add ava test

--- test.js
+++ test.js
@@ -1,14 +1,64 @@
-'use strict';
-var assert = require('assert');
-var gotPSK = require('./');
+import test from 'ava';
+import gotPSK from './';
+import mkdirp from 'mkdirp';
+import rimraf from 'rimraf';
+import fs from 'fs';
+import path from 'path';
 
-it('should', function (done) {
-	gotPSK().then(function (res) {
-		console.log(res);
-		done();
-	}).catch(function () {
-		console.log('error');
-		done();
+function access(file, cb) {
+	fs.access(path.join(__dirname, '.tmp', file), fs.F_OK, err => {
+		if (err) {
+			console.log(err);
+		}
+
+		cb(err);
 	});
-	assert(true);
+}
+
+test.before(t => {
+	rimraf.sync('.tmp');
+	t.end();
+});
+
+test('get the latest version of psk full version', t => {
+	mkdirp('.tmp/full');
+
+	gotPSK('.tmp/full', 'full').then(function () {
+		access('full/gulpfile.js', e => {
+			t.ok(!e);
+			t.end();
+		});
+	}).catch(function (err) {
+		t.fail(err);
+		t.end();
+	});
+});
+
+test('get the specific version of psk', t => {
+	mkdirp('.tmp/full-1.0.3');
+
+	gotPSK('.tmp/full-1.0.3', 'full@1.0.3').then(function (res) {
+		t.not(res.zip.indexOf('1.0.3'), -1);
+		access('full-1.0.3/gulpfile.js', e => {
+			t.ok(!e);
+			t.end();
+		});
+	}).catch(function (err) {
+		t.fail(err);
+		t.end();
+	});
+});
+
+test('get the latest of light', t => {
+	mkdirp('.tmp/light');
+
+	gotPSK('.tmp/light', 'light').then(function () {
+		access('light/app/bower_components', e => {
+			t.ok(!e);
+			t.end();
+		});
+	}).catch(function (err) {
+		t.fail(err);
+		t.end();
+	});
 });
